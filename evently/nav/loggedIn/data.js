8e68ab95e21f53b252aa16dba79d663284dfcbb0
Dale Harvey <dale@arandomurl.com>
2010-10-01 16:17:34 +0100

    interim

--- evently/nav/loggedIn/data.js
+++ evently/nav/loggedIn/data.js
@@ -1,6 +1,14 @@
 function(e, r) {
-  //console.log(e, r);
-  return {
-    me : r.userCtx.name
-  };
+  var data = { me : r.userCtx.name },
+      hash = window.location.hash.slice(1);
+  
+  var selected = (hash === "/items/" + r.userCtx) ? "mine"
+    : (hash === "/focus") ? "all"
+    : (hash === "/tags") ? "tags"
+    : (hash.indexOf("/team") !== -1) ? "team" : "";
+
+  console.log(selected);
+  data[selected] = "class='selected'";
+  
+  return data;
 };
