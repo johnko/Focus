7e692cdf1480eff18bc50963d4f47572240dc75e
Dale Harvey <dale@arandomurl.com>
2010-10-03 12:50:20 +0100

    highlight current navigation location

---
+++ _attachments/evently.init.js
@@ -0,0 +1,61 @@
+var Focus = (function () {
+  
+  function hashChanged(name) {
+    
+    var hash     = window.location.hash.slice(1),
+        selected = (hash === "/items/" + name) ? "mine" // Ugly, tmp
+      : (hash === "/focus") ? "all"
+      : (hash === "/tags") ? "tags"
+      : (hash.indexOf("/team") !== -1) ? "team"
+      : (hash.indexOf("/items/") !== -1) ? "team" : false;
+    
+    $(".selected").removeClass("selected");
+    if (selected) { 
+      $("."+selected).addClass("selected");
+    }
+  };
+  
+  return {
+    hashChanged : hashChanged
+  };
+})();
+
+$.evently.changesOpts.include_docs = true;
+$.evently.log = true;
+
+setTimeout(function() {
+    
+  $.couch.app(function(app) {
+    
+    $("#account").evently("account", app);
+    $("#profile").evently("profile", app);
+    $("#nav").evently("nav", app);
+    $("#items").evently("items", app);
+    
+    $.evently.connect("#account","#profile", ["loggedIn","loggedOut"]);
+    $.evently.connect("#account","#nav", ["loggedIn","loggedOut"]);
+    
+    $("#account").bind("loggedIn", function() {
+      $("body").trigger("evently-changes-"+app.db.name);
+    });
+    
+    $("#account").bind("loggedOut", function() {
+      $("body").trigger("evently-changes-"+app.db.name);
+    });
+    
+    $.pathbinder.onChange(function (){
+      Focus.hashChanged($$("#account").userCtx &&
+                        $$("#account").userCtx.name || "");
+    });
+    
+    $.pathbinder.begin("/focus");
+    
+    // Scroll past the url bar
+    setTimeout(function () {
+      $('html, body').animate({scrollTop: 1});
+    }, 200);
+  });
+  
+  // wish there was a better way to keep the URL bar from showing 
+  // loading forever on Android
+}, 100); 
--- evently/nav/loggedIn/data.js
+++ evently/nav/loggedIn/data.js
@@ -1,14 +1,6 @@
 function(e, r) {
-  var data = { me : r.userCtx.name },
-      hash = window.location.hash.slice(1);
-  
-  var selected = (hash === "/items/" + r.userCtx) ? "mine"
-    : (hash === "/focus") ? "all"
-    : (hash === "/tags") ? "tags"
-    : (hash.indexOf("/team") !== -1) ? "team" : "";
-
-  console.log(selected);
-  data[selected] = "class='selected'";
-  
-  return data;
+  setTimeout( function () { 
+    Focus.hashChanged(r.userCtx.name);
+  }, 0);
+  return { me :  r.userCtx.name };
 };
