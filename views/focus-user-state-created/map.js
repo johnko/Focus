3f1495d4875aeb3b6267f99537e4bfca33d0ec87
Dale Harvey <dale@arandomurl.com>
2010-10-05 10:53:30 +0100

    show unpublished items in focus view

--- views/focus-user-state-created/map.js
+++ views/focus-user-state-created/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-    if (doc.publish) { 
+    //if (doc.publish) { 
         emit([doc.profile.name, doc.state, (doc.edit_at || doc.state_at || doc.created_at)], doc);
-    }
+    //}
 };
