7e94c31bd31e354aa00db0244677377c16f2afa1
Dale Harvey <dale@arandomurl.com>
2010-10-11 23:24:15 +0100

    minor code cleanup

--- _attachments/focus.js
+++ _attachments/focus.js
@@ -1,15 +1,15 @@
 var Focus = (function () {
 
-  var dbName = "focus",
-      router = new Router(),
-      user   = null,
-      profile = null,
+  var dbName   = "focus",
+      router   = new Router(),
+      user     = null,
       profiles = [],
-      users = null,
-      db     = $.couch.db(dbName);
+      db       = $.couch.db(dbName);
 
   var xhrCache = {},
       docCache = {};
+
+  var selected = "selected='selected'";
   
   router.pre(urlChange);
 
@@ -33,13 +33,11 @@ var Focus = (function () {
   router.get("!/logout", function (id) {
     $.couch.logout({
       success : function() {
-        user = null;
         document.location.href = "";
       }
     });
   });
     
-  // should also combine these
   router.get("", function () {
     window.location.hash = "#!";
   });
@@ -84,23 +82,19 @@ var Focus = (function () {
     });
   });
   
-  router.post("login", function (e) {
+  router.post("login", function (e, data) {
     $.couch.login({
-      name     : $(e.target).find("[name='name']").val(),
-      password : $(e.target).find("[name='password']").val(),
-      success  : function() {
-        window.location.reload(true);
-      },
-      error    : function() {
-        notifyMsg("Invalid Login Credentials");
-      }
+      name     : data.name,
+      password : data.password,
+      success  : function() { window.location.reload(true); },
+      error    : function() { notifyMsg("Invalid Login Credentials"); }
     });
   });
-
+  
   router.post("edit", function(e, data) {
     var doc = docCache[data._id];
     doc.message = data.message;
-    doc.state = data.state;
+    doc.state   = data.state;
     doc.profile = getProfile(data.assigned);
     doc.blocked = data.blocked == "on";
     doc.publish = data.publish == "on";
@@ -109,7 +103,7 @@ var Focus = (function () {
     db.saveDoc(doc, {
       success : function(r) {
         window.location.hash = getRedirectUrl();
-        notifyMsg("Updated: "+doc.message);
+        notifyMsg("Updated");
       }
     });
   });
@@ -117,34 +111,30 @@ var Focus = (function () {
   router.post("create", function(e) {
     var doc = {
       created_at : new Date(),
-      profile : getProfile(user.userCtx.name),
-      publish : false,
-      message : $("#message").val(),
-      state : "now",
-      type : "task"
+      profile    : getProfile(user.userCtx.name),
+      publish    : true,
+      message    : $("#message").val(),
+      state      : "now",
+      type       : "task"
     };
     doc.profile.name = user.userCtx.name;
     db.saveDoc(doc, {
       success : function(r) {
         $("#message").val("");
-        notifyMsg('New item: </span>'+doc.message);
+        notifyMsg("Added new item");
       }
     });
   });
   
-  router.post("delete", function (e) {
-    var doc = {
-      _id  : $(e.target).find("[name='_id']").val(),
-      _rev : $(e.target).find("[name='_rev']").val()
-    };
-    db.removeDoc(doc, {
+  router.post("delete", function (e, data) {
+    db.removeDoc({_id: data._id, _rev: data._rev}, {
       success:function() {
         notifyMsg("deleted");
         window.location.hash = getRedirectUrl();
       }
     });  
   });
-
+  
   function getRedirectUrl() {
     var arr = window.location.hash.split("/");
     arr.pop();
@@ -154,19 +144,15 @@ var Focus = (function () {
   
   function notifyMsg(msg) {
     $("#notify").html('<span/>').html(msg).show();
-    setTimeout(function() { 
-      $("#notify").fadeOut();
-    }, 3000);    
+    setTimeout(function() { $("#notify").fadeOut(); }, 3000);    
   };
   
   function createEdit(id) { 
     fetchId(id, function(data) {
-      fetchUsers(data.profile.name, function(users) {
-        data.users = users;
-        data.states = makeState(data.state);
-        render("#content", "#edit_tpl", data);
-        $("textarea[name=message]")[0].focus();
-      });
+      data.users  = selectUsers(data.profile.name);
+      data.states = states(data.state);
+      render("#content", "#edit_tpl", data);
+      $("textarea[name=message]")[0].focus();
     });
   };
 
@@ -183,16 +169,20 @@ var Focus = (function () {
     fetchList("done", name, function(done) {
       fetchList("now", name, function(now) {
         fetchList("later", name, function(later) {
-          var tpl = $("#items_tpl").html();
-          var isSelf = name === user.userCtx.name;
+          
+          var isSelf    = name === user.userCtx.name,
+              tmpRender = function(view) {
+                return Mustache.to_html($("#items_tpl").html(), {
+                  urlPrefix : prefix,
+                  items     : viewToList(view, isSelf)
+                });
+              };
+          
           render("#content", "#overview_tpl", {
             profile : isSelf ? false : getProfile(name),
-            done  : Mustache.to_html(tpl, {urlPrefix:prefix,
-                                           items:viewToList(done, isSelf)}),
-            now   : Mustache.to_html(tpl, {urlPrefix:prefix,
-                                           items:viewToList(now, isSelf)}),
-            later : Mustache.to_html(tpl, {urlPrefix:prefix,
-                                           items:viewToList(later, isSelf)})
+            done    : tmpRender(done),
+            now     : tmpRender(now),
+            later   : tmpRender(later)
           });
         });
       });
@@ -201,29 +191,34 @@ var Focus = (function () {
   
   function fetchList(list, name, cb) {
     var args = (list === "done") ?
-      { descending:true,
-        startkey:JSON.stringify([name, list, {}]),
-        endkey:JSON.stringify([name, list, daysAgo(7)])
+      { descending : true,
+        startkey   : JSON.stringify([name, list, {}]),
+        endkey     : JSON.stringify([name, list, daysAgo(7)])
       } : {
-        descending:true,
-        endkey:JSON.stringify([name, list]),
-        startkey:JSON.stringify([name, list, {}]) };
+        descending : true,
+        endkey     : JSON.stringify([name, list]),
+        startkey   : JSON.stringify([name, list, {}]) };
 
     fetch("focus-user-state-created", args, cb);
   };
 
   function daysAgo(days) {
-    return new Date(new Date() - (24 * 60 * 60 * days * 1000));
+    var d = new Date();
+    return new Date(
+      new Date((d.getMonth()+1) + "/" + d.getDate() + "/" + d.getFullYear()
+               - (24 * 60 * 60 * days * 1000)));
   };
   
   function viewToList(data, isSelf) {
     for (var obj, tmp = [], i = 0; i < data.rows.length; i += 1) {
       obj = data.rows[i].value;
       docCache[obj._id] = cloneObj(obj);
-      obj.states = makeState(obj.state);
-      obj.message = linkUp(obj.message);
+      
+      obj.states    = states(obj.state);
+      obj.message   = linkUp(obj.message);
       obj.published = obj.publish ? "published" : "unpublished";
-      obj.isSelf = isSelf ? "isSelf" : "isNotSelf";
+      obj.isSelf    = isSelf ? "isSelf" : "isNotSelf";
+      
       if (isSelf || (!isSelf && obj.publish)) {
         tmp.push(obj);
       }
@@ -233,94 +228,40 @@ var Focus = (function () {
       
   function showTagsOrMentions(view, key) {
 
-    var args = {
-      descending : true,
-      reduce     : false,
-      endkey     : JSON.stringify([key]),
-      startkey   : JSON.stringify([key, {}])
-    };
+    var pre  = (view === "tags") ? "#" : "@",
+        args = {
+          descending : true,
+          reduce     : false,
+          endkey     : JSON.stringify([key]),
+          startkey   : JSON.stringify([key, {}])
+        };
     
     fetch(view, args, function(data) {
       render("#content", "#items_tpl", {
-        title     : "Viewing \"" + ((view === "tags") ? "#" : "@") + key + "\"",
+        title     : "Viewing '" + pre + key + "'",
         items     : viewToList(data),
         urlPrefix : "/" + view + "/" + key
       });
     });
   };
   
-  function fetchUsers(name, callback) {
+  function selectUsers(name) {
     for (var tmp = [], i = 0; i < profiles.length; i += 1) {
       tmp.push({
-        selected: (name === profiles[i].profile.name)
-          ? 'selected="selected"' : "",
-        profile:profiles[i].profile,
-        name:profiles[i].profile.name
+        selected : (name === profiles[i].profile.name) ? selected : "",
+        profile  : profiles[i].profile,
+        name     : profiles[i].profile.name
       });
     }
-    callback(tmp);
+    return tmp;
   };
   
-  function loadUsers(callback) {
-    
-    // Alternative version based on users table, fixes the "you have to have
-    // created a ticket to be assigned" bug, but doesnt replicate nicely
-    //   var f = function(users) {
-    //     for (var i = 0; i < users.length; i++) {
-    //       if (users[i].name === "name") {
-    //         users[i].selected = "selected='selected'";
-    //       }
-    //     }
-    //     callback(users);
-    //   };
-    
-    //   if (users) {
-    //     f(users.slice(0));
-    //   } else {
-    //     $.getJSON("/_users/_all_docs", {include_docs:true}, function(data) {
-    //       for (var arr = [], i = 0; i < data.rows.length; i++) {
-    //         if (data.rows[i].doc && data.rows[i].doc.type === "user") { 
-    //           arr.push(data.rows[i].doc);
-    //         }
-    //       }
-    //       users = arr;
-    //       f(users.slice(0));
-    //     });
-    //   }
-    // };
-    
-    // fetch("user-created", {group_level:1}, function(users) {
-    //   for (var tmp = [], i = 0; i < users.rows.length; i += 1) {
-    //     tmp.push({
-    //       selected: (name === users.rows[i].key[0]) ? 'selected="selected"' : "",
-    //       profile:users.rows[i].value.profile,
-    //       name:users.rows[i].key[0]
-    //     });
-    //   }
-    //   callback(tmp);
-    // });
-    
-    fetch("user-created", {group:true}, function(users) {
-      for (var tmp = [], i = 0; i < users.rows.length; i += 1) {
-        tmp.push(users.rows[i].value);
-      }
-      
-      $.post("/" + dbName + "/_all_docs?include_docs=true", JSON.stringify({keys:tmp}), function(data) {
-        for (i = 0; i < data.rows.length; i += 1) {
-          profiles.push(data.rows[i].doc);
-        }
-        callback();
-      }, "json");
-    });
-  };
-  
-  
-  function makeState(current) {
+  function states(current) {
     var states = ["done", "now", "later"];
     for (var arr = [], i = 0; i < states.length; i += 1) {
       arr.push({
-        state:states[i],
-        selected: (states[i] === current) ? 'selected="selected"' : ""
+        state    : states[i],
+        selected : (states[i] === current) ? selected : ""
       });
     }
     return arr;
@@ -351,7 +292,7 @@ var Focus = (function () {
   function fetch(view, opts, callback) {
 
     var id = view + JSON.stringify(opts),
-        url = "/" + dbName + "/_design/" + dbName + "/_view/" + view;
+        url = "/" + dbName + "/_design/focus/_view/" + view;
     
     if (typeof xhrCache[id] === "undefined") {
       $.get(url, opts, function (data) {
@@ -368,11 +309,13 @@ var Focus = (function () {
     if (verb === "GET") {
 
       $("#content").addClass("loading");
-      
-      var selected = (url === "!/") ? "navmine" :
+
+      // nasty way of figuring out what nav should be highlighted
+      // can do a nicer way
+      var selected = (url === "!/")   ? "navmine" :
         (url.indexOf("focus") !== -1) ? "navall" : 
-        (url.indexOf("team") !== -1) ? "navteam" : 
-        (url.indexOf("tags") !== -1) ? "navtags" : "navmine"; 
+        (url.indexOf("team") !== -1)  ? "navteam" : 
+        (url.indexOf("tags") !== -1)  ? "navtags" : "navmine"; 
       
       $(".selected").removeClass("selected");
       if (selected) {
@@ -401,32 +344,42 @@ var Focus = (function () {
   
   function loadUser() {
     $.getJSON("/_session/", function (data) {
-      if(data && data.userCtx && data.userCtx.name !== null) {
+      if (data && data.userCtx && data.userCtx.name !== null) {
         user = data;
-        loadUsers(function () {
-          router.init();
-        });
+        loadUsers(router.init);
       } else { 
         router.init();
       }
     });
   };
   
-  // this must be a nasty way of invalidating views, cant really
-  // see if incremental view updates are possible
+  function loadUsers(callback) {
+    fetch("user-created", {group:true}, function(users) {
+      for (var keys = [], i = 0; i < users.rows.length; i += 1) {
+        keys.push(users.rows[i].value);
+      }
+      var url  = "/" + dbName + "/_all_docs?include_docs=true",
+          args = JSON.stringify({keys:keys});
+      $.post(url, args, function(data) {
+        for (i = 0; i < data.rows.length; i += 1) {
+          profiles.push(data.rows[i].doc);
+        }
+        callback();
+      }, "json");
+    });
+  };  
+  
   function badComet(seq) {
-    var url = "/" + dbName + "/_changes?heartbeat=10000&" +
-      "&feed=longpoll&since=" + seq;
     $.ajax({
-      url      : url,
+      url      : "/" + dbName + "/_changes",
+      data     : {heartbeat: 10000, feed:"longpoll", since: seq},
       method   : "GET",
       dataType : "json",
-      error    : function() { },
       success  : function(data) {
         if (data) { 
           xhrCache = {};
           docCache = {};
-          // Bit of a hack
+          // Bit of a hack, dont refresh the form while people are editing
           if (window.location.hash.indexOf("/edit/") === -1) {
             router.refresh();
           }
@@ -438,20 +391,20 @@ var Focus = (function () {
 
   function initComet() { 
     db.info({
-      "success": function(data) {
-        setTimeout(function() { 
-          badComet(data.update_seq);
-        }, 100);
+      "success": function (data) {
+        setTimeout(function () { badComet(data.update_seq); }, 100);
       }
     });
   };
 
+  // I dont like these global events, they are bound to the page permanently
+  // so may cause conflicts
   function bindEvents() {
     
     $(document).bind("mousedown", function (e) {
-      // can get rid of this now that items dont have embedded forms
       var item = $(e.target).is("div.item")
-        ? $(e.target) : $(e.target).parents("div.item");
+        ? $(e.target)
+        : $(e.target).parents("div.item");
       if (e.target.nodeName !== "A" && item.length !== 0) {
         document.location.hash =
           document.location.hash + "/edit/" + item.attr("data-id");
@@ -470,8 +423,6 @@ var Focus = (function () {
   bindEvents();
 
   // Scroll past the url bar
-  setTimeout(function () {
-    $('html, body').animate({scrollTop: 1});
-  }, 200);
+  setTimeout(function () { $('html, body').animate({scrollTop: 1}); }, 200);
   
 })();
