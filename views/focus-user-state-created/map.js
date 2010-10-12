1287c57d7b0b0488ce6e507947dd0a105460e5c9
Dale Harvey <dale@arandomurl.com>
2010-10-13 00:46:22 +0100

    order now/later items by create date, done by edit date

--- views/focus-user-state-created/map.js
+++ views/focus-user-state-created/map.js
@@ -1,5 +1,11 @@
 function(doc) {
-    //if (doc.publish) { 
-  emit([doc.profile.name, doc.state, (doc.created_at)], doc);
-    //f}
+  // Order now / later items consistently so they dont jump around the ui
+  // done items are ordered by last edit to the last week view makes sense
+  if (doc.type && doc.type === "task") {
+    if (doc.state === "now" || doc.state === "later") { 
+      emit([doc.profile.name, doc.state, (doc.created_at)], doc);
+    } else {
+      emit([doc.profile.name, doc.state, (doc.edit_at || doc.created_at)], doc);
+    }
+  }
 };
